acd28b8bb4f6b31033cadeeb297caae6e25e4b13
Ganesh Bendi <ganesh@Ganeshs-MacBook-Air.local>
2024-03-02 16:37:03 +0530

    Add header component and experiments component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { CrewComponent } from "./crew/crew.component";
+import { TechnologyComponent } from "./technology/technology.component";
+import { ExperimentsComponent } from "./experiments/experiments.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone:true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, CrewComponent, TechnologyComponent, ExperimentsComponent]
 })
 export class AppComponent {
-  title = 'Apple';
+  title = 'Apple Mac';
 }
